b6389aa1d1d811f661e891af20a42aec3702d10e
Alan <anhlando@yahoo.com>
2017-10-30 14:05:14 +0700

    Add for__statement.py and print_odds.py

---
+++ for_statement.py
@@ -0,0 +1,8 @@
+#program to print from 1 to 20
+for i in range(1,20):
+        if i==7 or i==15:
+                continue
+        elif i==18:
+                break
+        else:
+                print (i)	 
---
+++ print_odds.py
@@ -0,0 +1,8 @@
+#print all the odd numbers from 0 to 200
+
+for i in range (201):
+    if i%2==0:
+        continue
+    else:
+        print (i)
+        
