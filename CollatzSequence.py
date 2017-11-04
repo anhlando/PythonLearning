30625da691dd955ccaad0ea6f0bbd72a31329db2
Alan <anhlando@yahoo.com>
2017-11-04 10:15:55 +0700

    Modified CollatzSequence.py: add a report about the attemps made to reach 1

--- CollatzSequence.py
+++ CollatzSequence.py
@@ -6,9 +6,12 @@ def collatz(number):
 
 
 print('Input an integer to start the Collatz sequence: ',end=" ")
+count = 0
 i = int(input())
 while i != 1:
+    count = count + 1
     i = collatz(i)
     print(str(i))
 
+print('Total attemp(s) to reach 1: ' + str(count))
 
