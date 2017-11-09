2a7f78f6e2305e3d9c831f1984957407045c6933
Alan <anhlando@yahoo.com>
2017-11-09 13:47:47 +0700

    Modified learn_dictionaries.py

--- learn_dictionaries.py
+++ learn_dictionaries.py
@@ -1,7 +1,22 @@
 myDog = {'gender':'male','color':'white','size':'medium','age':'7'}
-for item in myDog:
-    print(myDog[item])
+#for item in myDog:
+print(myDog.keys())
+print(myDog.values())
+print(myDog.items())
 
+#birthday = {'dad':'16/05','mom':'24/09','son':'15/03','daughter':'19/01'}
 
-lista = ['dog','cat','mouse','duck','chicken','bird','fish']
-print(lista[-2])
+# while True:
+#     print('Enter a name(leave blank to exit): ',end='')
+#     name=input()
+#     if name=='':
+#         break
+#
+#     if name in birthday:
+#         print('Birthday of ' + name + ' is ' + birthday[name])
+#     else:
+#         print('Cannot find ' + name + ' in database. Please provide birthday info for ' + name + ' to update to database: ',end='')
+#         bday = input()
+#         birthday[name] = bday
+#         print('Birthday database updated successfully!!!')
+
